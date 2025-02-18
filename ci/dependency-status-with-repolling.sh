6c4b95ba4c49fdb4dad3ea802f1ef5e1174d8ce2
Michael Seaton <mseaton@pih.org>
2025-02-18 10:55:23 -0500

    Improve logging

--- ci/dependency-status-with-repolling.sh
+++ ci/dependency-status-with-repolling.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+echoWithDate() {
+  CURRENT_DATE=$(date '+%Y-%m-%d-%H-%M-%S')
+  echo "${CURRENT_DATE}: ${1}"
+}
+
 hasChecked=0
 hasChanged=0
 needsRecheck=0
@@ -25,12 +30,12 @@ declare -A local_dependencies=()
 declare -A remote_dependencies=()
 
 # Determine the modules for which the dependency report will be run and needs to be checked
-echo "List of modules to check for dependency changes:"
+echoWithDate "List of modules to check for dependency changes:"
 moduleBuildDirs=$(mvn ${mavenArgs} -q --also-make exec:exec -Dexec.executable="echo" -Dexec.args='${project.build.directory}')
-echo "$moduleBuildDirs"
+echoWithDate "$moduleBuildDirs"
 
 build_dependency_report() {
-  echo "Building the latest version of the dependency report"
+  echoWithDate "Building the latest version of the dependency report"
   mvn ${mavenArgs} --batch-mode --no-transfer-progress clean process-resources -U
 }
 
@@ -39,7 +44,7 @@ build_dependency_report
 
 # Get the latest remote dependency report for each module
 for moduleBuildDir in ${moduleBuildDirs}; do
-  echo "Checking module at ${moduleBuildDir}"
+  echoWithDate "Checking module at ${moduleBuildDir}"
   module=$(dirname "${moduleBuildDir}")
   moduleBuildDir=$(echo "${moduleBuildDir}" | sed 's/\\/\//g')
   pomFile=$(dirname "${moduleBuildDir}")/pom.xml
@@ -52,16 +57,16 @@ for moduleBuildDir in ${moduleBuildDirs}; do
   if [ -f "${moduleBuildDir}/${filename}" ]; then
     remoteDependencyReportDir="${moduleBuildDir}/remote-dependency-report"
     remoteDependencyReportPath="${remoteDependencyReportDir}/${filename}"
-    echo "Downloading remote dependency report for ${artifact}"
+    echoWithDate "Downloading remote dependency report for ${artifact}"
     mvn ${mavenArgs} --batch-mode --no-transfer-progress dependency:get -Dartifact=${artifact} -Dtransitive=false -U
     mvn ${mavenArgs} --batch-mode --no-transfer-progress dependency:copy -Dartifact=${artifact} -DoutputDirectory="${remoteDependencyReportDir}/"  -Dmdep.useBaseVersion=true
     if [ -f "${remoteDependencyReportPath}" ]; then
       remote_dependencies[${module}]=$(md5sum ${remoteDependencyReportPath} | awk '{ print $1 }')
-      echo "Computed md5 for remote dependency: ${remote_dependencies[${module}]}"
+      echoWithDate "Computed md5 for remote dependency: ${remote_dependencies[${module}]}"
     fi
     local_dependency_filenames[${module}]="${moduleBuildDir}/${filename}"
   else
-    echo "No dependency report for this module, skipping"
+    echoWithDate "No dependency report for this module, skipping"
   fi
 done
 
@@ -70,17 +75,17 @@ do
   needsRecheck=0
   for moduleBuildDir in ${moduleBuildDirs}; do
     module=$(dirname "${moduleBuildDir}")
-    echo "Checking module: ${module}"
+    echoWithDate "Checking module: ${module}"
     local_dependency_file=${local_dependency_filenames[${module}]}
 
     if [ -f "${local_dependency_file}" ]; then
 
       remote_dependency_checksum=${remote_dependencies[${module}]}
-      echo "Remote checksum: ${remote_dependency_checksum}"
+      echoWithDate "Remote checksum: ${remote_dependency_checksum}"
 
       # Compare the latest generated report with the remote report, to see if there are local changes
       local_dependency_checksum=$(md5sum "${local_dependency_file}" | awk '{ print $1 }')
-      echo "Local checksum: ${local_dependency_checksum}"
+      echoWithDate "Local checksum: ${local_dependency_checksum}"
       if [ "${remote_dependency_checksum}" != "${local_dependency_checksum}" ]; then
         hasChanged=1
       fi
@@ -88,7 +93,7 @@ do
       # If there are local changes, then see if this is a recheck by comparing the latest saved local checksums
       if [ ${hasChanged} -eq 1 ]; then
         previous_local_checksum="${local_dependencies[${module}]}"
-        echo "Previous local checksum: ${previous_local_checksum}"
+        echoWithDate "Previous local checksum: ${previous_local_checksum}"
         if [ "${previous_local_checksum}" != "${local_dependency_checksum}" ]; then
           needsRecheck=1
         fi
@@ -104,7 +109,7 @@ do
   fi
 
   if [ ${hasChanged} -eq 1 ] && [ ${needsRecheck} -eq 1 ] ; then
-    echo "Dependencies have changed.  Re-checking for additional changes in ${recheckDelay} seconds."
+    echoWithDate "Dependencies have changed.  Re-checking for additional changes in ${recheckDelay} seconds."
     sleep ${recheckDelay}
     build_dependency_report
   fi
@@ -113,9 +118,9 @@ do
 done
 
 if [ ${hasChanged} -eq 0 ]; then
-  echo "Check completed: No changes detected"
+  echoWithDate "Check completed: No changes detected"
 else
-  echo "Check completed: Dependency changes detected"
+  echoWithDate "Check completed: Dependency changes detected"
 fi
 
 exit ${hasChanged}
